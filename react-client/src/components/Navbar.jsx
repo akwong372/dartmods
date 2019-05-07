fbe6f5264be0aa2522a43b4c8b7091330091627b
Alvin Kwong <akwong372@gmail.com>
2019-05-07 12:03:40 -0700

    Add dropdown menu, work on search bar

--- react-client/src/components/Navbar.jsx
+++ react-client/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Navbar = () => (
   <div>
-    <nav className="navbar navbar-expand-lg" id='navbar'>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark" id='navbar'>
       <div className='container-fluid'>
         <span className="navbar-brand h1" id='navbarTitle'>Page Title</span>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -13,16 +13,15 @@ const Navbar = () => (
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <div className='nav-item dropdown mr-sm-2'>
-            <button className='btn btn-secondary dropdown-toggle nav-link' type='button' id='navbarDropdown' data-toggle='dropdown'>click</button>
+            <button className='btn btn-secondary dropdown-toggle nav-link' type='button' id='navbarDropdown' data-toggle='dropdown'>Order By: </button>
             <div className='dropdown-menu' id='navbarDropdownInner'>
-              <button className='dropdown-item' type='button' id='navbarDropdownItem'>1</button>
-              <button className='dropdown-item' type='button' id='navbarDropdownItem'>2</button>
-              <button className='dropdown-item' type='button' id='navbarDropdownItem'>3</button>
+              <button className='dropdown-item' type='button' id='navbarDropdownItem'>New</button>
+              <button className='dropdown-item' type='button' id='navbarDropdownItem'>Likes</button>
             </div>
           </div>
-          <form className='form-inline'>
+          <form className='navbar-nav form-inline'>
             <button className='btn btn-outline-success my-2 my-sm-0'>Search</button>
-            <input className='form-control ml-sm-2' type="search" placeholder="Search Title..."></input>
+            <input className='form-control ml-sm-2' type="search" placeholder="Search Tags..."></input>
           </form>
         </div>
       </div>
