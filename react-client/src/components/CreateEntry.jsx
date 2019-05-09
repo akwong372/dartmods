d4ba5b7b034454c3eeb97575258e88752f780696
akwong372 <akwong372@gmail.com>
2019-05-09 16:09:14 -0700

    Add styling for posting modal

--- react-client/src/components/CreateEntry.jsx
+++ react-client/src/components/CreateEntry.jsx
@@ -7,21 +7,49 @@ const createEntry = (props) => (
         <div className="modal-header">
           <h4 className='modal-title'>Submission Form</h4>
         </div>
-        <div className="modal-body">
-          <form action="">
-            <input type="text" id='submitTitle' required />
-            <input type="text" id='submitDescription' required />
-            <input type="text" id='submitParts' required />
-            <input type="text" id='submitTags' required />
-            <label htmlFor="submitMain">Post contents:</label>
 
-            <textarea id="submitMain" name="submitMain"
-              rows="5" cols="90" placeholder='What would you like to type?' wrap='soft' required>
-            </textarea>
-            <hr />
-            <div className="submissionButtonContainer">
-              <button type="button" className="btn btn-secondary mx-sm-1" data-dismiss="modal">Cancel</button>
-              <button type="submit" className='btn btn-success mx-sm-1'>Submit</button>
+        <div className="modal-body">
+          <form className='px-sm-1' action="">
+            <div className="container-fluid" id='formContainer'>
+
+              <div className="col formElement" id='titleContainer'>
+                <label htmlFor="submitTitle">Title:</label>
+                <br />
+                <input type="text" id='submitTitle' placeholder='Title here...' size='30' required />
+              </div>
+
+              <div className="col formElement" id='descriptionContainer'>
+                <label htmlFor="submitDescription">Description:</label>
+                <br />
+                <input type="text" id='submitDescription' placeholder='Description here...' size='30' required />
+              </div>
+
+              <div className="col formElement" id='partsContainer'>
+                <label htmlFor="submitParts">Parts/tools list:</label>
+                <br />
+                <input type="text" id='submitParts' placeholder='k26' size='30' />
+              </div>
+
+              <div className="col formElement" id='TagsContainer'>
+                <label htmlFor="submitTags">Tags:</label>
+                <br />
+                <input type="text" id='submitTags' placeholder='tag1 tag2 etc...' size='30' required />
+              </div>
+
+              <div className="col" id='submitMainContainer'>
+                <label htmlFor="submitMain">Post contents:</label>
+                <textarea id="submitMain" name="submitMain"
+                  rows="5" cols="90" placeholder='What would you like to type?' wrap='soft' required>
+                </textarea>
+              </div>
+
+
+              <hr />
+              <div className="submissionButtonContainer">
+                <button type="button" className="btn btn-secondary mx-sm-1" data-dismiss="modal">Cancel</button>
+                <button type="submit" className='btn btn-success mx-sm-1'>Submit</button>
+              </div>
+
             </div>
           </form>
         </div>
