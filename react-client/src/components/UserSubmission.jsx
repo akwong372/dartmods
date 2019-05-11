867fd21faba07591f7babad303cabefa5fe83f27
akwong372 <akwong372@gmail.com>
2019-05-11 13:27:20 -0700

    Moves user submission modal to own component

--- react-client/src/components/UserSubmission.jsx
+++ react-client/src/components/UserSubmission.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import UserSubmissionModal from './UserSubmissionModal.jsx';
+import UserSubmissionModal from './UserSubmissionModal.jsx';
 
 const userSubmission = (props) => (
   <div className='col-md-3'>
@@ -8,33 +8,28 @@ const userSubmission = (props) => (
         <h4>
           {JSON.parse(props.title)}
         </h4>
-        <div className='container'>
-          <div>Likes: {props.likes}</div>
-          <div>Date: {new Date(Date.parse(props.date)).toDateString()}</div>
+        <div className='container text-center'>
+          <div className='row'>
+            <div className='col'>Likes: {props.likes}</div>
+            <div className='col'>Date: {new Date(Date.parse(props.date)).toDateString()}</div>
+          </div>
           <div>Description: {JSON.parse(props.description)}</div>
         </div>
       </div>
     </div>
 
-    <div className='modal fade container-fluid' id={'userSubmissionModal' + props.postNumber} tabIndex='-1' role='dialog'>
-      <div className='modal-dialog modal-dialog-centered modal-lg'>
-        <div className="modal-content">
-          <div className="modal-header">
-            <h4 className='modal-title'>{JSON.parse(props.title)}</h4>
-            <h5>Author: {props.author}</h5>
-          </div>
-          <div className="modal-body">
-            <div>Likes: {props.likes}</div>
-            <div>Date: {new Date(Date.parse(props.date)).toDateString()}</div>
-            <div>Description: {JSON.parse(props.description)}</div>
-            <div>Parts: {JSON.parse(props.parts)}</div>
-            <div>Tags: {props.tags.join(', ')}</div>
-            <div className='mainText'>Main: {JSON.parse(props.main)}</div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div >
+    <UserSubmissionModal
+      postNumber={props.postNumber}
+      author={props.author}
+      likes={props.likes}
+      date={props.date}
+      title={props.title}
+      description={props.description}
+      parts={props.parts}
+      tags={props.tags}
+      main={props.main}
+    />
+  </div>
 );
 
 export default userSubmission;
